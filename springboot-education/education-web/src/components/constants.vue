79a7b0b1bff090b64d17b2a2164b6101ca81b59a
micheal <996770483@qq.com>
2022-01-12 22:07:06 +0800

    demo 菜单

--- springboot-education/education-web/src/components/constants.vue
+++ springboot-education/education-web/src/components/constants.vue
@@ -5,7 +5,7 @@
 
 <script>
   // const baseUrl= "http://localhost:8080/"
-  const baseUrl= "http://localhost:8080/";
+  const baseUrl= "http://localhost:8089/";
     export default {
         baseUrl
     }
