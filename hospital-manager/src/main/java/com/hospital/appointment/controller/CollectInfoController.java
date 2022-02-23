f8ce49f50742eff07e1178abc8b302bddebfac9a
bauhinia <958728255@qq.com>
2022-02-23 21:57:50 +0800

    insert

---
+++ hospital-manager/src/main/java/com/hospital/appointment/controller/CollectInfoController.java
@@ -0,0 +1,66 @@
+package com.hospital.appointment.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.hospital.appointment.entity.CollectInfo;
+import com.hospital.appointment.service.CollectInfoService;
+
+import java.io.Serializable;
+import java.util.List;
+
+import static com.hospital.appointment.utils.CommUtils.getTimestamp;
+
+/**
+ * <p>标题: 服务 - 请求控制层</p>
+ * <p>描述: </p>
+ * <p>版权: Copyright (c) 2022</p>
+ * <p></p>
+ *
+ * @version: 1.0
+ * @author: kknight
+ * @date 2022-2-23
+ */
+@RestController
+@RequestMapping("/collectInfo")
+public class CollectInfoController {
+
+    @Autowired
+    private CollectInfoService collectInfoService;
+
+    @GetMapping("/findList")
+    public List<CollectInfo> findList(CollectInfo collectInfo) {
+        return collectInfoService.findList(collectInfo);
+    }
+
+    @GetMapping("/findListByPage")
+    public Page<CollectInfo> findListByPage(CollectInfo collectInfo, Page page) {
+        QueryWrapper<CollectInfo> queryWrapper = new QueryWrapper<>(collectInfo);
+        return collectInfoService.page(page, queryWrapper);
+    }
+
+    @GetMapping("/{id}")
+    public CollectInfo getById(@PathVariable String id) {
+        return collectInfoService.getById(id);
+    }
+
+    @PostMapping("/save")
+    public Serializable save(@RequestBody CollectInfo collectInfo) {
+        collectInfo.setCreateTime(getTimestamp());
+        collectInfoService.save(collectInfo);
+        return collectInfo.getId();
+    }
+
+    @PostMapping("/edit")
+    public boolean edit(CollectInfo collectInfo) {
+        return collectInfoService.updateById(collectInfo);
+    }
+
+    @GetMapping("/delete/{id}")
+    public boolean removeById(@PathVariable Serializable id) {
+        return collectInfoService.removeById(id);
+    }
+
+}
